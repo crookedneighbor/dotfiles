9d26986fbb6271531d15c761a9cf5dd45d63683f
Blade Barringer <blade@crookedneighbor.com>
2023-03-11 08:11:49 -0600

    chore: clean up source annotations

--- zsh/source/functions.zsh
+++ zsh/source/functions.zsh
@@ -1,24 +1,24 @@
 # go to git root directory
-## http://stackoverflow.com/a/957978/1048479
+## @source: http://stackoverflow.com/a/957978/1048479
 function groot() {
   cd "$(git rev-parse --show-toplevel)"
 }
 
 # base64 encode a file, The result will be copied to the clipboard (macos only)
 ## @arg file_path: the path to the file you want to base64 encode
-## source: @mathias-bynens comment to  http://superuser.com/a/120815/146376
+## @source: @mathias-bynens comment to  https://superuser.com/a/120815/146376
 base64() {
   openssl base64 < $1 | tr -d '\n' | pbcopy
 }
 
 # make a directory and cd into it
 ## @arg directory: the directory to create and cd into
-## http://theunixgeek.blogspot.com/2009/02/merging-mkdir-and-cd.html
+## @source: https://theunixgeek.blogspot.com/2009/02/merging-mkdir-and-cd.html
 mcd() { [ -n "$1"  ] && mkdir -p "$1" && cd "$1";  }
 
 # Search Google for terms (macos only)
 ## @arg search_term: the search term to search for
-## http://superuser.com/questions/47192/google-search-from-linux-terminal
+## @source: https://superuser.com/questions/47192/google-search-from-linux-terminal
 google() {
     search=""
     for term in $*; do
@@ -41,7 +41,7 @@ serve() {
 }
 
 # cd into whatever is the forefront Finder window.
-## Stolen from paul irish's dotfiles: https://github.com/paulirish/dotfiles/commit/e67d1bc03
+## @source: paul irish's dotfiles: https://github.com/paulirish/dotfiles/commit/e67d1bc03
 cdf() {  # short for cdfinder
   cd "`osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)'`"
 }
