eac7838242dc07465a308b19d92911250a78b93d
Blade Barringer <blade@crookedneighbor.com>
2023-03-11 07:55:38 -0600

    chore: remove unused functions

--- zsh/source/functions.zsh
+++ zsh/source/functions.zsh
@@ -10,16 +10,6 @@ base64() {
   openssl base64 < $1 | tr -d '\n' | pbcopy
 }
 
-# copy
-## Copies User's sshkey to clipboard
-## @todo: have a general "copy" alias to use in linux + darwin?
-sshkey() {
-  if exists pbcopy; then
-    pbcopy < ${HOME}/.ssh/id_rsa.pub
-    echo "sshkey copied to clipboard. Have fun"
-  fi
-}
-
 # make a directory and cd into it
 # http://theunixgeek.blogspot.com/2009/02/merging-mkdir-and-cd.html
 mcd() { [ -n "$1"  ] && mkdir -p "$1" && cd "$1";  }
@@ -32,37 +22,6 @@ google() {
         search="$search%20$term"
     done
     $open "http://www.google.com/search?q=$search"
-}
-
-# Get a Python Module's directory (inspired by: http://stackoverflow.com/a/15211581/1048479)
-## optionally, cd to the given path by adding the optional argument "-go"
-pmodule() {
-  pmodule_path="$(dirname $(echo "t=__import__(\"$1\",fromlist=[\".\"]); print(t.__file__)" | python ))"
-  echo "$1 source: $pmodule_path
-  dir stored in ENV \$pmodule_path; use the '-go' flag to cd to directory"
-  if [[ "$2" == "-go" ]]; then
-    cd $pmodule_path
-  fi
-}
-
-# download index.html from HTML5BP
-html5() {
-  curl -O https://raw.github.com/h5bp/html5-boilerplate/master/index.html
-}
-
-# convenience to scale canvas of image (downscaling does not quite work)
-scaleCanvas() {
-  if exists convert; then
-    cp $1 $1.bak
-    mogrify -gravity center -background transparent -extent $2 $1
-  else
-    echo "convert not found, is ImageMagick installed and in your path?"
-  fi
-}
-
-# Check for existence of program
-exists () {
-  hash $1 2>/dev/null
 }
 
 # Start a python simple HTTP server. This is superseded, for the most part, by node's static (``npm install -g static``)
